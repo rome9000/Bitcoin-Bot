53d0b38da7f396ce66e9bb3a2c04a54e1f46fc18
AJ <akinjcampbell@gmail.com>
2023-08-05 10:37:51 -0500

    Add comments and directions

--- test_bot-aj.py
+++ test_bot-aj.py
@@ -1,14 +1,16 @@
 import coinbase
 from coinbase.wallet.client import Client
-#print("hello world")
+print("hello world")
 #data = open('passphrase', r).read().splitlines()
 #print("gb world")
 #print(data)
 
+# Coinbase information and object instantiation
+# To generate API key visit https://help.coinbase.com/en/exchange/managing-my-account/how-to-create-an-api-key
 coinbase_API_key = '9xF36UsqaybNxP2M'
 coinbase_API_secret = '6NXAzklQF2BwrlQivB0t6alFOmBXuqfW'
 client = Client(coinbase_API_key, coinbase_API_secret)
-print(client.get_current_user())
+print(client)
 
 # accounts = client.get_accounts()
 # assert isinstance(accounts.data, list)
